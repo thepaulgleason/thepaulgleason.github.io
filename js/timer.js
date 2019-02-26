dc8c62997c16b2080d2d6dfdb752a3890c01b51f
youngchap27 <pauljogleason@gmail.com>
2019-02-26 14:33:32 -0800

    timer-changed code to make it count down

--- js/timer.js
+++ js/timer.js
@@ -3,49 +3,42 @@
 //so we need to see if it is logged in
 
 
-var running = false
-var time = 25 //make time equal to firebase.user.activity.timeForActivity 
 
-function start(){
-    status = true;
-    document.getElementById("startBtn").disabled = true;
-    myTimer();
-}
-
-function stop(){
-    status = false;
-    document.getElementById("startBtn").disabled = true;
-}
- 
-function reset(){
-    status = false;
-    time = 25;
-}
-
-function myTimer() {
-  if (status == true) {
-    time--;
-    if(time == 0){
-        status = false;
-        //make time flash OR go to break
-    }
-  var min = Math.floor(time/100/60);
-  var sec = Math.floor(time/100);
-
-if(min<10){
-    min = "0" + min;
-}
-if(sec>=60){
-    sec = sec % 60;
-}
-if (sec<10){
-    sec = "0" + sec;
-} 
-
-document.getElementById("timerLabel").innerHTML = min + ":" + sec + ":";
-
-}
-  }
+            var status = 0; // 0:stop 1:running
+            var time = 0;
+            var startBtn = document.getElementById("startBtn");
+            var timerLabel = document.getElementById('timerLabel');
+          function start(){
+                status = 1;
+                startBtn.disabled = true;
+                timer();
+            }
+            function stop(){
+                status = 0;
+                startBtn.disabled = false;
+            }
+            function reset(){
+                status = 0;
+                time = 0;
+                timerLabel.innerHTML = '00:00:00';
+                startBtn.disabled = false;
+            }
+            function timer(){
+                if (status == 1) {
+                    setTimeout(function() {
+                        time++;
+                        var min = Math.floor(time/100/60);
+                        var sec = Math.floor(time/100);
+                        var mSec = time % 100;
+                        if (min < 10) min = "0" + min;
+                        if (sec >= 60) sec = sec % 60;
+                        if (sec < 10) sec = "0" + sec;
+                        if (mSec < 10) mSec = "0" + mSec;
+                        timerLabel.innerHTML = min + ":" + sec + ":" + mSec;
+                        timer();
+                    }, 10);
+            }
+         }
 
     
 
