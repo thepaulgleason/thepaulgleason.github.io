974c5c11e2f19607fc156d6a2ad85e52d45ba326
youngchap27 <pauljogleason@gmail.com>
2019-03-13 13:35:13 -0700

    added app.js and made comment to html page

---
+++ js/app.js
@@ -0,0 +1,18 @@
+//database
+(function(){
+      // FIREBASE
+  var config = {
+    apiKey: "AIzaSyCZzcxxtq48xiWnjKu5AbtmqbZYD0VyIYI",
+    authDomain: "focustomato.firebaseapp.com",
+    databaseURL: "https://focustomato.firebaseio.com",
+    projectId: "focustomato",
+    storageBucket: "focustomato.appspot.com",
+    messagingSenderId: "958241018007"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
+
+
+})
+  
+
