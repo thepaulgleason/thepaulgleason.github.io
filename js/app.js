a067702ed11dcbbaccdbc15435a60db31525cd92
youngchap27 <pauljogleason@gmail.com>
2019-03-14 13:37:51 -0700

    firestore attempt 8 or 9 - got rid of code from presvious tutorial, app.js essentially clear.

--- js/app.js
+++ js/app.js
@@ -1,27 +1,10 @@
 
-const activityList = document.querySelector('#activity-list')
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
 
-function renderUser(doc){
-let li = document.createElement('li');
-let activityName = document.createElement('span');
-let totalFocusTimeWorked = document.createElement('span');
 
-li.setAttribute('data-id', doc.id);
-name.textContext = doc.data().userActivity;
-totalFocusTimeWorked.textContent = doc.data().totalFocusTimeWorked;
 
-li.appendChild(activityName);
-li.appendChild(totalFocusTimeWorked);
-
-activityList.appendChild(li);
-
-}
-
-db.collection('user').get().then((snapshot) => {
-  snapshot.docs.forEach(doc => {
-    renderUser(doc);
-  });;
-})
   
   
 
