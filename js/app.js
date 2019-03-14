58198a64f1668da77dd97a6ab0617316e4f2cb8b
youngchap27 <pauljogleason@gmail.com>
2019-03-14 13:52:15 -0700

    firestore attempt 9- attempted to add a new user through js/app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,16 @@ const firebase = require("firebase");
 // Required for side-effects
 require("firebase/firestore");
 
-
+db.collection("users").add({
+  firstName: "Robert",
+  last: "Paulson"
+})
+.then(function(docRef) {
+  console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+  console.error("Error adding document: ", error);
+});
 
   
   
