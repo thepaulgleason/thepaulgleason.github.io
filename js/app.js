b7a6570eb7ce94b06df1aeb9fa6b9d700bd6a285
youngchap27 <pauljogleason@gmail.com>
2019-03-14 10:05:40 -0700

    firestore attempt 3- changed location of some of the javascript back to focusTomato.html

--- js/app.js
+++ js/app.js
@@ -1,24 +1,15 @@
 //database
 (function(){
       // FIREBASE
-  var config = {
-    apiKey: "AIzaSyCZzcxxtq48xiWnjKu5AbtmqbZYD0VyIYI",
-    authDomain: "focustomato.firebaseapp.com",
-    databaseURL: "https://focustomato.firebaseio.com",
-    projectId: "focustomato",
-    storageBucket: "focustomato.appspot.com",
-    messagingSenderId: "958241018007"
-  };
-  firebase.initializeApp(config);
   
 
   const preObject = document.getElementById('object');
 
-  const dbRefObject = firebase.firestore();
+  const db = firebase.firestore();
 
-  dbRefObject.settings({timestampsInSnapshots: true});
+  db.settings({timestampsInSnapshots: true});
 
-  dbRefObject.on('value', snap => console.log(snap.val()));
+  db.on('value', snap => console.log(snap.val()));
 
 
 
