fe662022cf060955540720b2ab4ed15db2b5a6a9
youngchap27 <pauljogleason@gmail.com>
2019-03-14 11:39:59 -0700

    firestore attempt 7- attempted to add youtube tutorial code. accesses firestore by calling the document i created in the firestore console

--- js/app.js
+++ js/app.js
@@ -1,6 +1,26 @@
 
+const activityList = document.querySelector('#activity-list')
+
+function renderUser(doc){
+let li = document.createElement('li');
+let activityName = document.createElement('span');
+let totalFocusTimeWorked = document.createElement('span');
+
+li.setAttribute('data-id', doc.id);
+name.textContext = doc.data().userActivity;
+totalFocusTimeWorked.textContent = doc.data().totalFocusTimeWorked;
+
+li.appendChild(activityName);
+li.appendChild(totalFocusTimeWorked);
+
+activityList.appendChild(li);
+
+}
+
 db.collection('user').get().then((snapshot) => {
-  console.log(snapshot.docs);
+  snapshot.docs.forEach(doc => {
+    renderUser(doc);
+  });;
 })
   
   
