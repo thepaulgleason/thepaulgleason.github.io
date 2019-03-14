85e7530b10bfa8a15c50c4b3cc6888ae5cc4d387
youngchap27 <pauljogleason@gmail.com>
2019-03-14 10:48:03 -0700

    firestore attempt 4- I added html elements, and moved all js code for firestore to html page for now

--- js/app.js
+++ js/app.js
@@ -1,15 +1,9 @@
 //database
 (function(){
       // FIREBASE
+
   
-
-  const preObject = document.getElementById('object');
-
-  const db = firebase.firestore();
-
-  db.settings({timestampsInSnapshots: true});
-
-  db.on('value', snap => console.log(snap.val()));
+  
 
 
 
